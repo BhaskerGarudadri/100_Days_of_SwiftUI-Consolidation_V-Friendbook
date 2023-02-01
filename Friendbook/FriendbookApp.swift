69c24a0d8e724862e7691fbb43eaa02b7a9b30a1
Edwin Przeźwiecki Jr <edwin.przezwiecki.jr@icloud.com>
2023-02-01 09:43:54 +0100

    Initial Commit

---
+++ Friendbook/FriendbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendbookApp.swift
+//  Friendbook
+//
+//  Created by Edwin Przeźwiecki Jr. on 01/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct FriendbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
